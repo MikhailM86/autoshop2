d2f91830694077c06d2c0940bb177694506af528
unknown <mihail.n.maslihov@gmail.com>
2025-08-02 20:06:09 +0500

    add some fix

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -2,7 +2,7 @@ from .models import Cart
 
 def cart_items_count(request):
     count = 0
-    if hasattr(request, 'user') and request.user.is_authenticated:
+    if request.user.is_authenticated:
         cart = Cart.objects.filter(user=request.user).first()
         count = cart.items.count() if cart else 0
     return {'cart_items_count': count}
