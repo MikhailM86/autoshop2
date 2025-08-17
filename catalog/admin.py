6c02b8eb722f817e5fc32a25ca170cefca7a1096
unknown <mihail.n.maslihov@gmail.com>
2025-08-17 15:12:49 +0500

    fix some error

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,6 +7,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "brand", "part_number", "price", "stock")
     search_fields = ("name", "part_number")
     list_filter = ("brand", "category")
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('slug',)
 
 admin.site.register(Brand)
 admin.site.register(Category)
--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 import os
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Brand(models.Model):
@@ -19,6 +20,7 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
@@ -26,39 +28,56 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     part_number = models.CharField(max_length=50)  # Уникальный артикул
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True, null=True, default='')
-    image = models.ImageField(upload_to="products/", blank=True, null=True, default='images/products/default.png')
+    image = models.ImageField(upload_to="products/", blank=True,
+                              null=True, default='images/products/default.png')
     created_at = models.DateTimeField(auto_now_add=True)
     specs = models.CharField(blank=True, null=True, default='')
 
     def __str__(self):
         return f"{self.brand.name} {self.name}" if self.brand else self.name
-    
+
+def save(self, *args, **kwargs):
+    if not self.slug:
+        self.slug = orig = slugify(self.name)
+        counter = 1
+        while Product.objects.filter(slug=self.slug).exists():
+            self.slug = f"{orig}-{counter}"
+            counter += 1
+    super().save(*args, **kwargs)
+
+def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id, self.slug])
 
 # Сигналы для работы с файлами
+
+
 @receiver(post_delete, sender=Product)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             instance.image.delete(save=False)
 
+
 @receiver(pre_save, sender=Product)
 def auto_delete_file_on_change(sender, instance, **kwargs):
     if not instance.pk:
         return
-    
+
     try:
         old_file = Product.objects.get(pk=instance.pk).image
     except Product.DoesNotExist:
         return
-    
+
     new_file = instance.image
     if old_file and old_file != new_file:
         if os.path.isfile(old_file.path):
